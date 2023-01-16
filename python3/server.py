2fb6099defd69b834ca6f83910d136ed116e8795
Daniil Diachenko <d.diachenko@prequelapp.com>
2023-01-16 17:40:48 +0400

    Adds variable port

--- python3/server.py
+++ python3/server.py
@@ -17,6 +17,7 @@ cache = Cache(app)
 
 base_url = os.environ["BASE_URL"]
 base_port = os.environ["BASE_PORT"]
+need_port = bool(os.environ["NEED_PORT"])
 
 hash_encoder = hashlib.md5()
 
@@ -29,7 +30,7 @@ def generate_url():
     origin_url = str(request.query_string).split("=")[1][:-1]
     hash_url = generate_hash(origin_url)
     redis_client.set(hash_url, origin_url)
-    return base_url + ":" + base_port + "/go?url=" + hash_url
+    return base_url + f":{base_port}" if need_port else "" + "/go?url=" + hash_url
 
 @app.route("/go", methods=["GET"])
 def go():
