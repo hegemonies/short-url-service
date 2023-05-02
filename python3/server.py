87907337f9f39cb6b6230092fb7e90ea5d93228e
daniil dyachenko <hegemonies@yandex.ru>
2023-05-02 14:51:37 +0400

    Fixes of logic

--- python3/server.py
+++ python3/server.py
@@ -1,9 +1,12 @@
+from datetime import timedelta
 from flask import Flask, request, redirect
 from flask_caching import Cache
 import hashlib
 from dotenv import dotenv_values
 import redis
 import os
+import urllib
+from urllib.parse import urlparse
 
 config = dotenv_values(".env")
 
@@ -19,16 +22,26 @@ base_url = os.environ["BASE_URL"]
 
 hash_encoder = hashlib.md5()
 
+
 def generate_hash(url: str) -> str:
     hash_encoder.update(bytes(url, 'utf-8'))
     return hash_encoder.hexdigest()
 
+
+def get_address_from_url(query_string: str) -> str:
+    return urllib.parse.unquote(query_string).split("=")[1][:-1]
+
+
 @app.route("/generate", methods=["GET"])
 def generate_url():
-    origin_url = str(request.query_string).split("=")[1][:-1]
+    origin_url = get_address_from_url(request.query_string)
+    print(f"origin_url = {origin_url}")
     hash_url = generate_hash(origin_url)
-    redis_client.set(hash_url, origin_url)
+    key = hash_url
+    redis_client.set(key, origin_url)
+    redis_client.expire(key, timedelta(minutes=1))
     return base_url + "/go?url=" + hash_url
+
 
 @app.route("/go", methods=["GET"])
 def go():
